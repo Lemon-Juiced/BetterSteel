f6bd1bedddff96b57ec02abe216482b7dcbc1198
Lemon-Juiced <tbrotheawesome@gmail.com>
2023-06-08 23:43:56 -0400

    1.0.2 - Updated Mappings

--- src/main/java/lemon_juice/better_steel/block/ModBlocks.java
+++ src/main/java/lemon_juice/better_steel/block/ModBlocks.java
@@ -5,6 +5,7 @@ import lemon_juice.better_steel.item.ModItems;
 import net.minecraft.world.item.BlockItem;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.block.DropExperienceBlock;
 import net.minecraft.world.level.block.state.BlockBehaviour;
 import net.minecraftforge.eventbus.api.IEventBus;
@@ -17,9 +18,9 @@ import java.util.function.Supplier;
 public class ModBlocks {
     public static final DeferredRegister<Block> BLOCKS = DeferredRegister.create(ForgeRegistries.BLOCKS, BetterSteel.MOD_ID);
 
-    public static final RegistryObject<Block> MANGANESE_ORE = registerBlock("manganese_ore", () -> new DropExperienceBlock(BlockBehaviour.Properties.m_284310_().requiresCorrectToolForDrops().strength(3.0F, 3.0F)));
+    public static final RegistryObject<Block> MANGANESE_ORE = registerBlock("manganese_ore", () -> new DropExperienceBlock(BlockBehaviour.Properties.copy(Blocks.IRON_ORE).requiresCorrectToolForDrops().strength(3.0F, 3.0F)));
     public static final RegistryObject<Block> DEEPSLATE_MANGANESE_ORE = registerBlock("deepslate_manganese_ore", () -> new DropExperienceBlock(BlockBehaviour.Properties.copy(MANGANESE_ORE.get()).requiresCorrectToolForDrops().strength(4.5F, 3.0F)));
-    public static final RegistryObject<Block> RAW_MANGANESE_BLOCK = registerBlock("raw_manganese_block", () -> new Block(BlockBehaviour.Properties.m_284310_().requiresCorrectToolForDrops().strength(5.0F, 6.0F)));
+    public static final RegistryObject<Block> RAW_MANGANESE_BLOCK = registerBlock("raw_manganese_block", () -> new Block(BlockBehaviour.Properties.copy(Blocks.RAW_IRON_BLOCK).requiresCorrectToolForDrops().strength(5.0F, 6.0F)));
 
 
 
